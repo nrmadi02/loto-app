a72bc5918505d08b304fa658aff8962f1646d043
nrmadi02 <ahmadinur2202@gmail.com>
2025-09-01 22:02:29 +0800

    refactor: add service auth

--- src/app/api/[[...route]]/routers/auth.router.ts
+++ src/app/api/[[...route]]/routers/auth.router.ts
@@ -1,41 +1,21 @@
-import { ORPCError } from "@orpc/client";
 import { signupSchema } from "@/features/auth/types/auth-request.type";
-import { auth } from "@/lib/auth";
 import { publicProcedure } from "../lib/orpc";
+import { AuthService } from "../services/auth.service";
 
 export const authRouter = {
   signUp: publicProcedure
     .input(signupSchema)
     .handler(async ({ input, context }) => {
-      const db = context.prisma;
-      const data = await auth.api.signUpEmail({
-        body: {
-          name: input.name,
-          email: input.email,
-          password: input.password,
-        },
-      });
+      const service = new AuthService(context);
 
-      if (!data.user) {
-        throw new ORPCError("INTERNAL_SERVER_ERROR", {
-          message: "User not created",
-        });
-      }
-
-      const createEmployee = await db.employee.create({
-        data: {
-          userId: data.user.id,
-          employeeId: input.employeeId,
-          department: input.department,
-        },
-      });
+      const { user, employee } = await service.signUpEmail(input);
 
       return {
         status: "success",
         message: "User registered successfully",
         data: {
-          user: data.user,
-          employee: createEmployee,
+          user,
+          employee,
         },
       };
     }),
---
+++ src/app/api/[[...route]]/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { ORPCError } from "@orpc/client";
+import type { SignupFormData } from "@/features/auth/types/auth-request.type";
+import { auth } from "@/lib/auth";
+import type { Context } from "../lib/context";
+
+export class AuthService {
+  ctx: Context;
+
+  constructor(ctx: Context) {
+    this.ctx = ctx;
+  }
+
+  async signUpEmail(input: SignupFormData) {
+    const { name, email, password, employeeId, department } = input;
+
+    const data = await auth.api.signUpEmail({
+      body: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    if (!data.user) {
+      throw new ORPCError("INTERNAL_SERVER_ERROR", {
+        message: "User not created",
+      });
+    }
+
+    const employee = await this.ctx.prisma.employee.create({
+      data: { userId: data.user.id, employeeId, department },
+    });
+
+    return { user: data.user, employee };
+  }
+}
